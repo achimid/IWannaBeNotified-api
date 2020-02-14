a9c5de0f2813e976d976dd76d6866f47ae43b131
Lourran Zorzi <lourranzorzi@softbox.com.br>
2020-02-14 13:42:12 -0300

    adicionando sthealf mode e alguns plugins do puppeteer. Alem de atualizar a verçao nova

--- src/config/startup.js
+++ src/config/startup.js
@@ -1,7 +1,13 @@
-const puppeteer = require('puppeteer')  
+const puppeteer = require('puppeteer-extra')  
+const stealthPlugin = require('puppeteer-extra-plugin-stealth')
+const adblockerPlugin = require('puppeteer-extra-plugin-adblocker')
+
 const databaseInit = require('./database')
 const notifyJobInit = require('../cron/notify-job')
 const { telegramStartup } = require('../notification/telegram/telegram')
+
+puppeteer.use(stealthPlugin())
+puppeteer.use(adblockerPlugin())
 
 const initBrowser = async () => {
     console.info('Inicializando browser......')
@@ -10,6 +16,9 @@ const initBrowser = async () => {
             args: [
                 '--no-sandbox', 
                 '--disable-setuid-sandbox',
+                '--disable-infobars',
+                '--ignore-certifcate-errors',
+                '--ignore-certifcate-errors-spki-list',
                 // '--proxy-server=http://5.196.255.171:3128'
             ]
         });
