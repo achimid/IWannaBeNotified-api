e0caa86a5e525d671bf6164ff952d1f1d0ba919b
Lourran <achimid@hotmail.com>
2020-02-16 18:25:52 -0300

    criando alguns testes com socket

--- README.md
+++ README.md
@@ -9,21 +9,3 @@
 	Para o monitoramento de uma pagina, é possivel informar um script para obter o html da pagina ou extrair qualquer outra informação por meio de scripts.
 
 ## Notificação
-
-
-heroku URL: https://scraper-jquery-api.herokuapp.com/api/evaluate (POST)
-heroku URL: https://scraper-jquery-api.herokuapp.com/api/hash (POST)
-
-{
-	"url": "https://horriblesubs.info",
-	"options": {
-		"script": "const links = []; $('.latest-releases a').each(function() { links.push($(this)[0].href) }); links;",
-		"waitTime": 500,
-		"importJquery": true
-	}
-	
-}
-
-{
-	"url": "http://www.google.com"
-}
--- src/index.js
+++ src/index.js
@@ -7,6 +7,23 @@ const monitor = require('express-status-monitor')
 const statup = require('./config/startup')
 
 const app = express()
+const server = require('http').createServer(app)
+const socket = require('socket.io')(server)
+
+// socket.on('connection', (client) => { 
+//     console.log('Evento Connection')
+
+//     setTimeout(function (){
+//         console.log('evento emitido')
+//         socket.emit('client_download', {
+//             url : 'magnet:?xt=urn:btih:S2TOSWKHO3LBC5LZHU57DKXGN7SRZ4HY&tr=http://nyaa.tracker.wf:7777/announce&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://tracker.internetwarriors.net:1337/announce&tr=udp://tracker.leechersparadise.org:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://open.stealth.si:80/announce&tr=udp://p4p.arenabg.com:1337/announce&tr=udp://mgtracker.org:6969/announce&tr=udp://tracker.tiny-vps.com:6969/announce&tr=udp://peerfect.org:6969/announce&tr=http://share.camoe.cn:8080/announce&tr=http://t.nyaatracker.com:80/announce&tr=https://open.kickasstracker.com:443/announce'
+//         })
+//     }, 1000)
+    
+//     client.on('disconnect', () => console.log('disconecteddddd'))
+// })
+
+
 
 app.use(monitor())
 app.use(compression())
@@ -23,4 +40,4 @@ require('./config/healthcheck')(prefix)(app)
 // ================
 
 statup()
-app.listen(process.env.PORT)
+server.listen(process.env.PORT)
