cf48b146b5d913a7486354d04a1af591b1df30c9
Lourran <achimid@hotmail.com>
2020-02-17 09:22:22 -0300

    some code

--- src/index.js
+++ src/index.js
@@ -10,21 +10,6 @@ const app = express()
 const server = require('http').createServer(app)
 const socket = require('socket.io')(server)
 
-// socket.on('connection', (client) => { 
-//     console.log('Evento Connection')
-
-//     setTimeout(function (){
-//         console.log('evento emitido')
-//         socket.emit('client_download', {
-//             url : 'magnet:?xt=urn:btih:S2TOSWKHO3LBC5LZHU57DKXGN7SRZ4HY&tr=http://nyaa.tracker.wf:7777/announce&tr=udp://tracker.coppersurfer.tk:6969/announce&tr=udp://tracker.internetwarriors.net:1337/announce&tr=udp://tracker.leechersparadise.org:6969/announce&tr=udp://tracker.opentrackr.org:1337/announce&tr=udp://open.stealth.si:80/announce&tr=udp://p4p.arenabg.com:1337/announce&tr=udp://mgtracker.org:6969/announce&tr=udp://tracker.tiny-vps.com:6969/announce&tr=udp://peerfect.org:6969/announce&tr=http://share.camoe.cn:8080/announce&tr=http://t.nyaatracker.com:80/announce&tr=https://open.kickasstracker.com:443/announce'
-//         })
-//     }, 1000)
-    
-//     client.on('disconnect', () => console.log('disconecteddddd'))
-// })
-
-
-
 app.use(monitor())
 app.use(compression())
 app.use(express.json())
@@ -37,6 +22,13 @@ const prefix = process.env.API_PREFIX
 require('./site-request/sr-controller')(prefix)(app)
 require('./site-execution/se-controller')(prefix)(app)
 require('./config/healthcheck')(prefix)(app)
+// ================
+
+
+// WebSocket registration
+// ================
+global.socket = socket // TODO: Melhorar esta parte
+require('./notification/websocket/websocket-interface')
 // ================
 
 statup()
