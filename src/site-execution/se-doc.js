6f1bf6c5fa7ac06b92e98e77cf65f6ab971c2972
Lourran <achimidd@gmail.com>
2020-03-21 18:19:37 -0300

    swager e doc

---
+++ src/site-execution/se-doc.js
@@ -0,0 +1,15 @@
+/**
+ * Endpoint para testar a execução de uma request
+ *
+ * @group SiteExecution
+ * @route GET /api/v1/execute 
+ * @returns {SiteExecution.model} 200 - OK
+ */
+
+/**
+ * Endpoint para testar a execução de uma request
+ *
+ * @group SiteExecution
+ * @route POST /api/v1/execute 
+ * @returns {SiteExecution.model} 200 - OK
+ */
---
+++ src/site-request/sr-doc.js
@@ -0,0 +1,32 @@
+/**
+ * Endpoint para listar as request cadastradas
+ *
+ * @group SiteRequest
+ * @route GET /api/v1/notify 
+ * @returns {SiteRequest.model} 200 - OK
+ */
+
+ /**
+ * Endpoint para cadastrar uma nova request
+ *
+ * @group SiteRequest
+ * @route POST /api/v1/notify 
+ * @returns {SiteRequest.model} 200 - OK
+ */
+
+
+ /**
+ * Endpoint para editar uma request
+ *
+ * @group SiteRequest
+ * @route PUT /api/v1/notify/{id}
+ * @returns {SiteRequest.model} 200 - OK
+ */
+
+ /**
+ * Endpoint para executar uma request
+ *
+ * @group SiteRequest
+ * @route POST /api/v1/notify/{id}/execute
+ * @returns {SiteRequest.model} 200 - OK
+ */
