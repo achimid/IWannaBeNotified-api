b45d3045bcbf92950b40b834a5a54ddc01e8caa7
Lourran <achimidd@gmail.com>
2020-03-21 20:19:05 -0300

    incrementando a doc do swagger

--- src/site-request/sr-doc.js
+++ src/site-request/sr-doc.js
@@ -1,3 +1,31 @@
+
+/**
+ * @typedef Options
+ * @property {integer} _id
+ * @property {number} hitTime - Tempo entre as execuções (Defualt: De 3 em 3 minutos)
+ * @property {boolean} onlyChanged - Apenas envia a notificação quando o hashTarget for direfente do anterior
+ * @property {boolean} onlyUnique - Apenas envia a notificação quando o hashTarget for unico
+ * @property {boolean} useJquery - Injeta o JQuery na pagina antes de executar o script
+ * @property {number} waitTime - Aguarda uma determinada quantidade de tempo antes da execução do script
+ * @property {boolean} isDependency
+ * 
+ */
+
+/**
+ * @typedef SiteRequest
+ * @property {integer} _id
+ * @property {string} url.required - Url do site para ser monitorado
+ * @property {string} name - Nome do site monitorado
+ * @property {string} scriptTarget - Script para obter o alvo (Por padrão será o html da pagina)
+ * @property {Array.<string>} scriptContent -  Lista de Scripts para obter os conteudos do site
+ * @property {Options.model} options -  Opções de execução
+ * @property {SiteExecution.model} lastExecution - Resultado da ultima execução
+ * @property {Filter.model} filter - Filtro utilizado para busca de palavas similares
+ * @property {Array.<Notification>} notifications - Filtro utilizado para busca de palavas similares
+ */
+
+
+
 /**
  * Endpoint para listar as request cadastradas
  *
